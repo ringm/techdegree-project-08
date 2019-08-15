f4fc6d06351274c26acd25fec4a3aca7f34ff3ef
Martin <51160764+ringm@users.noreply.github.com>
2019-08-15 19:00:04 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,13 +3,15 @@
 //------------------------------
 
 const directory = document.querySelector('.directory');
-const employee = document.querySelector('.directory__item');
+const employeeList = document.querySelectorAll('.employee-list');
+const overlay = document.querySelector('.overlay');
+const closeIcon = document.querySelector('.icon-close')
 
 //------------------------------
 //FETCH REQUESTS
 //------------------------------
 
-fetch('https://randomuser.me/api/?results=12&&nat=us,ca,gb')
+fetch('https://randomuser.me/api/?results=12&&nat=us,ca,gb&inc=picture,name,email,location,phone,dob&noinfo')
     .then(response => response.json())
     .then(data => data.results)
     .then(results => results.forEach(result => generateEmployee(result)))
@@ -19,21 +21,44 @@ fetch('https://randomuser.me/api/?results=12&&nat=us,ca,gb')
 //------------------------------
 
 function generateEmployee(data) {
-    const li = document.createElement("li");
-    li.className = "directory__item";
-    const html = `
+    const bday = new Date(data.dob.date);
+    const basicInfoLi = document.createElement("li");
+    const aditionalInfoLi = document.createElement("li");
+    basicInfoLi.className = "directory__item";
+    aditionalInfoLi.setAttribute("id", `"${data.email}"`);
+    aditionalInfoLi.className = "window hidden";
+
+    const basicInfo = `
       <img class='avatar' src="${data.picture.large}" alt="${data.name.first} ${data.name.last}">
-      <div class='employee-data'>
-          <p class='name'>${data.name.first} ${data.name.last}</p>
-          <p class='email'>${data.email}</p>
-          <p class='location'>${data.location.city}</p>
+      <div class='employee-basic-info'>
+          <p class='employee-name capitalize'>${data.name.first} ${data.name.last}</p>
+          <p class='employee-data'>${data.email}</p>
+          <p class='employee-data capitalize'>${data.location.city}</p>
       </div>
     `;
-    li.innerHTML = html;
-    directory.appendChild(li);
+    basicInfoLi.innerHTML = basicInfo;
+    directory.appendChild(basicInfoLi);
+
+    const aditionalInfo = `
+      ${basicInfo}
+      <div class='employee-additional-info'>
+        <p class='employee-data'>${data.phone}</p>
+        <p class='employee-data capitalize'>${data.location.street}, ${data.location.postcode}</p>
+        <p class='employee-data'>Birthday: ${bday.getDate()}/${bday.getMonth()+1}/${bday.getFullYear()}</p>
+      </div>
+    `;
+    aditionalInfoLi.innerHTML = aditionalInfo;
+    employeeList.appendChild(aditionalInfoLi);
 }
 
 //------------------------------
 //EVENT LISTENERS
 //------------------------------
 
+directory.addEventListener('click', e => {
+  if(e.target.tagName != 'UL') {
+    overlay.classList.remove('hidden');
+  }
+})
+
+//closeIcon.addEventListener('click', e => overlay.classList.add('hidden'))
